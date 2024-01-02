dfe1ce5c00b6f0013e391373830a4ce0fb049320
Joe Freeman <joe@freemansoft.com>
2024-01-02 18:57:08 -0500

    add example program in the demo package

---
+++ packages/demo/example/lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:demo/asset_stuff.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AssetManifest manifest = await doSomethingWithAssetManifest();
+
+  var assets = manifest.listAssets();
+
+  runApp(MyApp(assets: assets));
+}
+
+class MyApp extends StatelessWidget {
+  final Iterable<String>? assets;
+
+  const MyApp({super.key, this.assets});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(children: [Text('Assets'), Text('$assets')]),
+    );
+  }
+}
